3c7dc9f2f0dc32389f22128a9befe2e4ed798e32
Peter Otte <pbotte@uni-mainz.de>
2024-05-23 16:07:25 +0200

    plot added

--- io_utils.py
+++ io_utils.py
@@ -1,12 +1,15 @@
 import torch 
 import torchvision as tv
-
+import matplotlib.pyplot as plt
 
 def get_convert_mikly_way():
     !wget https://d2pn8kiwq2w21t.cloudfront.net/original_images/jpegPIA10748.jpg
 
     milky_way = tv.io.read_image("./jpegPIA10748.jpg").permute(1,2,0).to(torch.float32)
-    return milky_way / 255
+    milky_way = milky_way / 255
+    return milky_way
+
+plt.imshow(milky_way.detach().cpu().numpy())
 
 def jax_to_torch(jax_tensor):
     return torch.from_numpy(np.asarray(jax_tensor.copy()))
