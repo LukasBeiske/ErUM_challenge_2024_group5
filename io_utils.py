fbb2539f1542f0d5921ccccc7a40a7260f0c0d2c
Lukas Beiske <lukas.beiske@tu-dortmund.de>
2024-05-23 16:23:56 +0200

    Update io

--- io_utils.py
+++ io_utils.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 def get_convert_mikly_way():
     !wget https://d2pn8kiwq2w21t.cloudfront.net/original_images/jpegPIA10748.jpg
 
-    milky_way = tv.io.read_image("./jpegPIA10748.jpg").permute(1,2,0).to(torch.float32)
-    milky_way = milky_way / 255
-    return milky_way
+    milky_way = tv.io.read_image("./jpegPIA10748.jpg")
+    milky_way = tv.transforms.Resize(size=64)(milky_way)    
+    milky_way = milky_way.permute(1,2,0).to(torch.float32)
+    return milky_way / 255
 
 plt.imshow(milky_way.detach().cpu().numpy())
 
